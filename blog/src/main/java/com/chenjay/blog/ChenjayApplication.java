09363d626c9a93b1829a35202b7dcc7bffe1e36b
pengbing <2275246254@qq.com>
2018-09-13 17:20:03 +0800

    框架搭建

---
+++ blog/src/main/java/com/chenjay/blog/ChenjayApplication.java
@@ -0,0 +1,12 @@
+package com.chenjay.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChenjayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChenjayApplication.class, args);
+    }
+}
