9cdb24ec28ce4ff4e7e0219a61a7c2b64ba15164
pengbing <2275246254@qq.com>
2018-09-14 17:29:13 +0800

    项目修改

---
+++ blog/src/main/java/com/chenjay/blog/dto/MetaDto.java
@@ -0,0 +1,16 @@
+package com.chenjay.blog.dto;
+
+import com.chenjay.blog.modal.Vo.MetaVo;
+
+public class MetaDto extends MetaVo {
+
+    private int count;
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
